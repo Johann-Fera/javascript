db1e05dbc8da4fc0027a7a56e79bf7f352975f24
Johann-Fera <johannarruda.dev2024@gmail.com>
2025-03-13 15:11:05 -0300

    será que termino isso hoje

--- js/mt_coisa.js
+++ js/mt_coisa.js
@@ -25,6 +25,22 @@ function imc(){
     let n2 = parseFloat(document.getElementById("num2").value);
     let formula = n1 / (n2 * n2);
     document.getElementById("resultado").textContent = "IMC: " + formula;
-
-
+}
+function idade(){
+    let nome = document.getElementById("nome").value;
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let faxa = "bebê"
+    if (n1 >= 5){
+        faxa = "criança"
+    } 
+    if (n1>=10){
+        faxa = "adolescente"
+    } 
+    if (n1>=20){
+        faxa = "adulto"
+    } 
+    if (n1>=60){
+        faxa = "idoso"
+    }
+    document.getElementById("resultado").textContent = nome + " é " + faxa;
 }
