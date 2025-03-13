792cb6c38d4d355bf1f0ade99081df9543a5dfb3
Johann-Fera <johannarruda.dev2024@gmail.com>
2025-03-13 14:06:06 -0300

    tem uma porra mais por vir

---
+++ js/mt_coisa.js
@@ -0,0 +1,17 @@
+function parimpar() {
+    let n1 = parseFloat(document.getElementById("num1").value);
+    let resto = n1 % 2;
+    if (resto == 0){
+        document.getElementById("resultado").textContent = "par";
+    } else{
+        document.getElementById("resultado").textContent = "impar";
+    }
+}
+function media(){
+    let n1 = parseFloat(document.getElementById("nota1").value);
+    let n2 = parseFloat(document.getElementById("nota2").value);
+    let n3 = parseFloat(document.getElementById("nota3").value);
+    let n4 = parseFloat(document.getElementById("nota4").value);
+    let medeia = (n1 + n2 + n3 + n4) / 4
+    document.getElementById("resultado").textContent = "Resultado: " + medeia;
+}
